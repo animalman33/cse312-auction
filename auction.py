58f6fe3ea030bc13eca12b934b0b754a4ee79135
Timothy Li <tli58@buffalo.edu>
2023-05-02 22:46:18 -0400

    Corrected implementation of joining/leaving rooms + adding bid

--- auction.py
+++ auction.py
@@ -1,35 +1,14 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, session, request
 from flask_login import LoginManager, current_user
 from src.database import Database
 from src.user import User
-from flask_socketio import SocketIO, send, join_room, leave_room
+from flask_socketio import SocketIO, join_room, leave_room, rooms, emit, send, broadcast
 from main import *
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-socketio.run(app)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-# Get session user
-@login_manager.user_loader
-def user_loader(id):
-    with Database() as DB:
-        user = DB.get_user_id(id)
-        if user:
-            return User(user[0], int(user[2]))
-        return None
-
-user = user_loader(id)
-
-# Redirects non-users to login
-@login_manager.unauthorized_handler
-def unauthorized():
-    if user == None:
-        return redirect("/login")
-    else:
-        pass
+users = {}
 
 # Render page
 @app.route('/')
@@ -40,32 +19,33 @@ def index():
 # Join a auction room
 @socketio.on('join')
 @current_user
-def join_auction(user):
-    username = user['username']
-    auction_room = user['room']
-    join_room(auction_room)
-    send(username + ' has joined.', to=auction_room)
+def join_auction(user, auctionID):
+    users[user] = request.sid
+    join_room(auctionID)
+    send("You have entered!", to=auctionID)
+    emit("feed", {user} + ' has joined.', broadcast=True)
 
 # Leave a auction room
 @socketio.on('leave')
 @current_user
-def leave_auction(user):
-    username = user['username']
-    auction_room = user['room']
-    leave_room(auction_room)
-    send(username + ' has left.', to=auction_room)
-
-# See new bids
-@socketio.on('bid')
-@current_user
-def new_bid(json):
-    print("Bid" + str(json))
+def leave_auction(user, auctionID):
+    leave_room(auctionID)
+    send("You have left!", to="/user/home")
+    emit("feed", {user} + ' has left.', broadcast=True)
 
 # Send a bid
-@socketio.on('bid')
+@socketio.on('new_bid')
 @current_user
-def handle_bid(json):
-    send(json)
+def new_bid(amount):
+    # Gets the user based off session ID
+    username = None
+    for user in users:
+        if users[user] == request.sid:
+            username = user
+    # Add bid and send to feed
+    Database.add_bid(username, amount, rooms(request.sid), Database.get_user_info(username)[2])
+    emit("feed", {username} + ' has bid ' + {str(amount)}, broadcast=True)
 
 if __name__ == "__main__":
     app.run("0.0.0.0", port=8080, debug=True)
+    socketio.run(app)
